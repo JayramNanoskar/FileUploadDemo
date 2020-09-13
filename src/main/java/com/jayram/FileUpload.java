b720a91bcc82f229175cd98f8103a43778bf8777
JayramNanoskar <jayram.n978@gmail.com>
2020-09-14 01:52:40 +0530

    adding file upload functionality

--- src/main/java/com/jayram/FileUpload.java
+++ src/main/java/com/jayram/FileUpload.java
@@ -1,11 +1,19 @@
 package com.jayram;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
 public class FileUpload extends HttpServlet {
@@ -14,6 +22,24 @@ public class FileUpload extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		PrintWriter out = response.getWriter();
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		try 
+		{
+			List<FileItem> items = upload.parseRequest(request);
+			Iterator<FileItem> iterator = items.iterator();
+			while (iterator.hasNext())
+			{
+				FileItem item = iterator.next();
+				item.write(new File("E:\\Jayram Java Project\\JSP and Servlet\\FileUploadDemo\\"+item.getName()));
+				out.println("File uploaded !!!");
+			}
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
-<h2>Hello World!</h2>
-	<form action="" method="post" enctype="multipart/form-data">
+<h2>File Upload</h2>
+	<form action="upload" method="post" enctype="multipart/form-data">
 		<input type="file" name="file" multiple="multiple" required="required"><br><br>
 		<input type="submit">
 	</form>
